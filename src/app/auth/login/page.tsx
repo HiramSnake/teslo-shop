ca38587bdaad11ca81d2e3d1ef16e9dc0676477c
hiram <hiram.galarza47@outlook.com>
2024-04-10 14:25:02 -0600

    updateed

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="">
       <h1>Login Page!!ss</h1>
-      <h2>Github and Gitlab 2</h2>
+      <h2>Github and Gitlab 3</h2>
    
      
     </main>
