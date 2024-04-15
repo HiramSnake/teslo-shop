904838a8812d1d2746ab18f93fd9c66ed9cd297a
HiramSnake <144142609+HiramSnake@users.noreply.github.com>
2024-04-15 13:40:12 -0600

    Menu created

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,6 +1,8 @@
+'use client'
 import { Product } from "@/interfaces"
 import Image from "next/image"
 import Link from "next/link"
+import { useState } from "react"
 
 
 interface Props{
@@ -8,16 +10,23 @@ interface Props{
 }
 
 export const ProductGridItem = ({product}: Props) => {
+
+    const [displayImage, setDisplayImage] = useState(product.images[0])
+
   return (
     <div className="rounded-md overflow-hidden fade-in">
         <Link href={`/product/${product.slug}`}>
         <Image
-        src={`/products/${product.images[0]}`} alt={product.title} className="w-full object-cover" width={500} height={500}
+        src={`/products/${displayImage}`} alt={product.title}
+         className="w-full object-cover rounded" 
+         width={500} height={500}
+         onMouseEnter={() => setDisplayImage(product.images[1])}
+         onMouseLeave={() => setDisplayImage(product.images[0])}
         />
         </Link>
 
         <div className="p-4 flex flex-col">
-            <Link href={`/product/${product.slug}`} >{product.title}</Link>
+            <Link className="hover:text-blue-600" href={`/product/${product.slug}`} >{product.title}</Link>
 
         </div>
 
